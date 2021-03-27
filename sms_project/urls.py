47e6b920daea9835180677e45bd3b229311c8ae3
Preetam Raj <iitianpreetam@gmail.com>
2021-03-27 17:50:55 +0530

    small change

--- sms_project/urls.py
+++ sms_project/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('', include('sms_app.urls'))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
