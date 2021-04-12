3039b0ddb839af02a8553ab9f1241103cfc5b14c
Preetam Raj <iitianpreetam@gmail.com>
2021-04-12 13:35:13 +0530

    Updated staff details

--- sms_app/HODurls.py
+++ sms_app/HODurls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
     
     path('add_user/', views.register_user_view, name='register_user'),
-    path('add_staff/', views.add_staff, name='add_staff'),
+#     path('add_staff/', views.add_staff, name='add_staff'),
     path('test/', views.test_view, name='test-view'),
     path('charts/', views.charts_view, name='charts_view'),
     path('login/', views.login_view, name='login_view'),
