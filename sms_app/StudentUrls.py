4d69c6fcff93652f3ecb1261322d6ac36e8ce947
RaviKumar7870 <b19191@students.iitmandi.ac.in>
2021-05-16 21:53:07 +0530

    Student Task Done

--- sms_app/StudentUrls.py
+++ sms_app/StudentUrls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import Student_views
 
 urlpatterns = [
-    path("", Student_views.student_home_view, name='student_home'),
-    path("student_view_attendence", Student_views.student_view_attendence_view, name='student_view_attendence'),
-    path("student_result", Student_views.student_result_view, name='student_result'),
-    path("student_apply_leave/<str:pk>", Student_views.student_apply_leave_view, name='student_apply_leave'),
-    path("student_send_feedback", Student_views.student_send_feedback_view, name='student_send_feedback'),
+    path("", Student_views.dashboard_view, name='dashboard'),
+    path("attendence", Student_views.attendence_view, name='attendence'),
+    path("result", Student_views.result_view, name='result'),
+    path("leave", Student_views.leave_view, name='leave'),
+    path("feedback", Student_views.feedback_view, name='feedback'),
 ]
 
      
