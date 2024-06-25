31b766a1690f2a7e40869be7bb6df33f12272e0d
Preetam Raj <iitianpreetam@gmail.com>
2024-06-25 21:56:47 +0530

    configured settings for render

--- sms_project/settings.py
+++ sms_project/settings.py
@@ -18,9 +18,12 @@ SECRET_KEY = os.getenv(key="SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['prp-sms.herokuapp.com', '127.0.0.1','*']
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
